ed592554b519dc58cbd243dd41c0c5c10b9aa628
IbooGamez <33536406+IbooGamez@users.noreply.github.com>
2019-03-12 10:51:05 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = 'p!'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'help') {
+    msg.reply('The bot prefix is **p!** and here are the commands: **ip, website, store**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'ip') {
+    msg.reply('The IP is **PiratesHCF.club**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'website') {
+    msg.reply('The Website is **https://piratehcf.enjin.com**');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'store') {
+    msg.reply('The Website is **https://piratehcf.buycraft.net**');
+  }
+});
+
+client.login(process.env.NTU0Nzc5Njk3OTAxMzM4NjQ1.D2hobw.YlKWeeJ_TpNDBiHPuAVpz752sl4);
